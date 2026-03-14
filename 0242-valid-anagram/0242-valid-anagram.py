f89d94dc0a45acf8b01858a0d5962f383a7b09dc
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-03-14 14:20:51 +0530

    Time: 15 ms (87.56%), Space: 12.4 MB (95.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,16 @@
 from collections import Counter
 class Solution(object):
     def isAnagram(self, s, t):
-        if(Counter(s)==Counter(t)):
-            return True
-        return False
-        
+        if len(s)!=len(t):
+            return False
+        dict={}
+        for i in s:
+            dict[i]=dict.get(i,0)+1
+        for i in t:
+            if i not in dict:
+                return False
+            dict[i]-=1
+        for i in dict:
+            if dict[i]!=0:
+                return False
+        return True
