0a9e60993b6dfbf14bfd618005c4457228e3a159
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-03-20 14:15:11 +0530

    Time: 3 ms (59.35%), Space: 13.1 MB (97.07%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,7 +3,7 @@ class Solution(object):
         if not nums:
             return 0
         i,j=0,1
-        while(i<j and j<len(nums)):
+        while(j<len(nums)):
             if nums[i]!=nums[j]:
                 i+=1
                 nums[i]=nums[j]
