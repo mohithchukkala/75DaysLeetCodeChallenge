aeeea421a7299f8e4cc4eba8a28f7dd620bdefda
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-07-19 15:33:40 +0530

    Time: 43 ms (33.02%), Space: 15.2 MB (25.6%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def subarraySum(self, nums, k):
-        presum=0
+        tot=0
         count=0
-        freq={}
-        freq[0]=1
-        for i in nums:
-            presum+=i
-            v=presum-k
-            if v in freq:
-                count+=freq[v]
-            if presum not in freq:
-                freq[presum]=1
-            else:
-                freq[presum]+=1
-        return count         
+        freq={0:1}
+        for i in range(len(nums)):
+            tot+=nums[i]
+            target=tot-k
+            if target in freq:
+                count+=freq.get(target,0)
+            freq[tot]= freq.get(tot,0)+1
+        return count
