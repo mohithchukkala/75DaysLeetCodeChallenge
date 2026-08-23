003e434764836498157ec3118bef194d65e87c64
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-08-23 13:04:48 +0530

    Time: 0 ms (100%), Space: 12.2 MB (91.92%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.py
@@ -3,27 +3,12 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def leng(head,count):
-        temp=head
-        while temp.next is not None:
-            temp=temp.next
-            count+=1
-        return count
-        
 class Solution(object):
     def middleNode(self, head):
-        count=1
-        l=leng(head,count)
-        curr=head
-        mid=l//2
-        if l%2==0:
-            while mid!=0:
-                curr=curr.next
-                mid-=1
-            return curr
-        else:
-            while mid!=0:
-                curr=curr.next
-                mid-=1
-            return curr
-    
+        slow=head
+        fast=head
+        while fast and fast.next is not None:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
+        
