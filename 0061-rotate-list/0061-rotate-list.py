ee2b2c80d8d153ec5986fad46cf19d80af193447
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-08-25 23:21:39 +0530

    Time: 10 ms (5.46%), Space: 12.6 MB (11.64%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.py
+++ 0061-rotate-list/0061-rotate-list.py
@@ -4,28 +4,31 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
-    def rotateRight(self, head,k):
+    def rotateRight(self, head, k):
         if head is None or head.next is None:
             return head
-        length=1
-        temp=head
-        while temp.next is not None:
-            temp=temp.next
-            length+=1
-        k=k%length
-        c=length-k
-        temp=head
-        if c==length:
+        slow=head
+        fast=head
+        count=1
+        while fast is not None and fast.next is not None:
+            fast=fast.next
+            count+=1
+        k=k%count
+        if k==0:
             return head
-        else:
-            while c!=1:
-                temp=temp.next
-                c-=1
-            curr=temp.next
-            new_head=curr
-            temp.next=None
-            while curr.next is not None:
-                curr=curr.next
-            curr.next=head
-            return new_head
-            
+        req=count-k
+        prev=None
+        while req:
+            prev=slow
+            slow=slow.next
+            req-=1
+        tail=slow
+        print(tail)
+        prev.next=None
+        while slow.next is not None:
+            slow=slow.next
+        slow.next=head
+        return tail
+        
+        
+        
