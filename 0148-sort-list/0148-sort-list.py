313bdbb1bf0ce08b887408bafa1b2e67dab04aea
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-08-28 21:44:23 +0530

    Time: 441 ms (30.35%), Space: 50.7 MB (50.81%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -3,40 +3,43 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def merge(lefthand,righthand):
+def merge(left,right):
     dummy=ListNode(0)
     tail=dummy
-    while lefthand and righthand:
-        if lefthand.val<righthand.val:
-            tail.next=lefthand
-            lefthand=lefthand.next
+    while left and right:
+        if left.val<right.val:
+            tail.next=left
+            left=left.next
         else:
-            tail.next=righthand
-            righthand=righthand.next
+            tail.next=right
+            right=right.next
         tail=tail.next
-
-    if lefthand:
-        tail.next=lefthand
-    if righthand:
-        tail.next=righthand
+    if left is not None:
+        tail.next=left
+    if right is not None:
+        tail.next=right
     return dummy.next
+                
+def length(head):
+    slow=head
+    fast=head
+    prev=None
+    while fast is not None and fast.next is not None:
+        prev=slow
+        slow=slow.next
+        fast=fast.next.next
+
+    prev.next=None
+    return slow
 def mergesort(head):
     if head is None or head.next is None:
         return head
     middle=length(head)
     left=mergesort(head)
     right=mergesort(middle)
+
     return merge(left,right)
-def length(head):
-    prev=None
-    slow=head
-    fast=head
-    while fast is not None and fast.next is not None:
-        prev=slow
-        slow=slow.next
-        fast=fast.next.next
-    prev.next=None
-    return slow
+
 class Solution(object):
     def sortList(self, head):
         return mergesort(head)
