40cfc5d531ce5fc68b2a8902f7373191f5ed3e3a
mohithchukkala <mohith2007.chukkala@gmail.com>
2026-09-18 12:07:49 +0530

    Time: 9 ms (58.81%), Space: 13.5 MB (74.55%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def majorityElement(self, nums):
+        
         ele=0
-        count=0
+        c=0
         for i in nums:
-            if count==0:
+            if c==0:
                 ele=i
             
             if i==ele:
-                count+=1
+                c+=1
             else:
-                count-=1
+                c-=1
         return ele
-        
